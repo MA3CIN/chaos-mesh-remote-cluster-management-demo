cb0a6e170fc61a4b0318d6c1b18335cb360c6005
MA3CIN <marcin.zet@onet.pl>
2023-08-04 13:58:01 +0200

    add context extract from kubeconfig

--- kind-setup-clusters.sh
+++ kind-setup-clusters.sh
@@ -1,3 +1,10 @@
 #Two k8s clusters - base (1 Control plane / 0 Workers) and external (1 Control plane / 1 Worker). 
 kind create cluster --config kind-base-cluster-config.yaml
 kind create cluster --config kind-external-cluster-config.yaml
+
+#Verfiy access to both clusters
+kubectl config get-contexts
+
+#retrieve kubeconfig from external cluster
+kubectl config use-context kind-external
+kubectl config view --raw --minify > kubeconfig.txt
