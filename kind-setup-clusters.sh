ce218a7f7f6864f46a25ca51047e0066b73c2581
MA3CIN <marcin.zet@onet.pl>
2023-08-09 08:55:25 +0200

    Fail script on first error

--- kind-setup-clusters.sh
+++ kind-setup-clusters.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 #Two k8s clusters - base (1 Control plane / 0 Workers) and external (1 Control plane / 1 Worker). 
 kind create cluster --config kind-base-cluster-config.yaml
 kind create cluster --config kind-external-cluster-config.yaml
