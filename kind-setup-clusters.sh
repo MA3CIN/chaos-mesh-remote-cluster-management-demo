947d2bcf686a395bfbb9160c3b03985c1952664c
MA3CIN <marcin.zet@onet.pl>
2023-08-09 08:58:34 +0200

    split kind cluster creation into separate script/dir

--- kind-setup-clusters.sh
+++ kind-setup-clusters.sh
@@ -2,8 +2,7 @@
 set -e
 
 #Two k8s clusters - base (1 Control plane / 0 Workers) and external (1 Control plane / 1 Worker). 
-kind create cluster --config kind-base-cluster-config.yaml
-kind create cluster --config kind-external-cluster-config.yaml
+./kind-files/create-clusters.sh
 
 #Verfiy access to both clusters
 kubectl config get-contexts
